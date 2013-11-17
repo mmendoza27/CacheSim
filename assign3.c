d27c78d8c395aa1298733311f7a5f78dba78e9dd
Michael Mendoza <mmendoza27@me.com>
2013-11-17 01:21:23 -0600

    Good stopping point for tonight. Seems like I just need to add things to array and start comparing.

--- assign3.c
+++ assign3.c
@@ -25,11 +25,14 @@ int main(int argc, char *argv[]) {
                     /* All the values we are calculating from the hits/misses */
    float missRatio = 0.0;
    int accesses = 0;
+   int associativity = 0;
    int hits = 0;
    int misses = 0;
    int oldTagValue;
 
-                     /* This turns on verbose mode. It is basically a boolean */
+       /* This turns on verbose, fifo, or lru mode. It is basically a boolean */
+   int fifo;
+   int lru;
    int tracing;
 
                      /* These variables hold the values needed to grab portions
@@ -37,12 +40,13 @@ int main(int argc, char *argv[]) {
    unsigned int blockNumber;
    unsigned int hexAddress;
    unsigned int tagValue;
+   unsigned int setNumber;
    unsigned int shiftValue;
 
                                 /* Check for the correct number of parameters */
-   if (argc != 5) {
-      fprintf(stderr, "Usage: %s <2^Cache Size> <2^Block Size> <Tracing On/Off>"
-      	" <Filename>\n", argv[0]);
+   if (argc != 7) {
+      fprintf(stderr, "Usage: %s <Cache Size> <Block Size> <Associativity> "
+        "<Tracing>\n\t<Replacement Policy> <Filename>\n", argv[0]);
       return 1;
    }
 
@@ -63,10 +67,28 @@ int main(int argc, char *argv[]) {
    int offset = atoi(argv[2]);
    //int tag = 32 - index - offset;
 
+                       /* Set the associativity (0 = direct, 1 = 2-way, etc.) */
+   if ((atoi(argv[3]) < 0) || (atoi(argv[3]) > index)) {
+      associativity = index;
+   } else {
+      associativity = atoi(argv[3]);
+   }
+
+                                   /* Set the replacement policy (fifo | lru) */
+   if (strcmp(argv[4], "fifo") == 0) {
+      fifo = TRUE;
+   } else if (strcmp(argv[4], "lru") == 0) {
+      lru = TRUE;
+   } else {
+      fprintf(stdout, "Please choose either \"FIFO\" or \"LRU\". "
+         "Exiting program\n");
+      return 1;
+   }
+
                           /* Turn on tracing based on the input from the user */
-   if (strcmp(argv[3], "on") == 0) {
+   if (strcmp(argv[5], "on") == 0) {
       tracing = TRUE;
-   } else if (strcmp(argv[3], "off") == 0) {
+   } else if (strcmp(argv[5], "off") == 0) {
       tracing = FALSE;
    } else {
       fprintf(stdout, "Enter \"on\" or \"off\". Exiting program.\n");
@@ -74,25 +96,46 @@ int main(int argc, char *argv[]) {
    }
 
              /* Determine if the file opens. If not, exit out of the program. */
-   if ((file = fopen(argv[4], "r")) == NULL) {
+   if ((file = fopen(argv[6], "r")) == NULL) {
       fprintf(stderr, "No file found.\n");
       return 1;
    }
 
                        /* Set up the array that holds the tags and init to -1 */
-   int cacheSize = pow(2, index);
+   int **cache;
+   int cacheSize;
    int i;
-   int cache[cacheSize];
+   int j;
+
+   if (associativity == 0 || associativity == index) {
+      cacheSize = pow(2, index);      
+      cache = (int **)malloc(cacheSize * sizeof(int *));
+
+      for (i = 0; i < cacheSize; i++) {
+        cache[i] = (int *)malloc(1 * sizeof(int));
+      }
+
+   } else {
+      cacheSize = pow(2, (index - associativity));
+      cache = (int **)malloc(cacheSize * sizeof(int *));
+
+      for (i = 0; i < cacheSize; i++) {
+        cache[i] = (int *)malloc(associativity * sizeof(int));
+      }
+
+   }
 
    for(i = 0; i < cacheSize; i++) {
-      cache[i] = -1;
+      for (j = 0; j < associativity; j++) {
+        cache[i][j] = -1;
+      }
    }
 
                                   /* Create the table header if tracing is on */
    if (tracing) {
       printf("*--------------------------------------------------------------");
       printf("---------------*\n");
-      printf("|%8s|%8s|%8s|%8s|", "Address", "Tag", "Block #", "Old Tag");
+      printf("|%8s|%8s|%8s|%8s|", "Address", "Tag", "Set #", "Old Tag");
       printf("%5s|%7s|%7s|%8s|", "Hit?", "Hits", "Misses", "Accesses");
       printf("%8s|\n", "Miss Ratio");
       printf("|==============================================================");
@@ -105,9 +148,16 @@ int main(int argc, char *argv[]) {
 
       accesses++;
 
-      shiftValue = pow(2, MAX_SZ) - pow(2, atoi(argv[1]));
+      shiftValue = pow(2, MAX_SZ) - pow(2, (atoi(argv[1]) - associativity));
       tagValue = hexAddress & shiftValue;
-      tagValue = tagValue >> atoi(argv[1]);
+      tagValue = tagValue >> (atoi(argv[1]) - associativity);
+
+      if (associativity == 0 || associativity == index) {
+        setNumber = 1;
+      } else {
+        setNumber = hexAddress >> (atoi(argv[2]));
+        setNumber = setNumber % (int)(pow(2, (index - associativity)));
+      }
 
       shiftValue = pow(2, atoi(argv[1])) - pow(2, offset);
       blockNumber = hexAddress & shiftValue;
@@ -115,11 +165,11 @@ int main(int argc, char *argv[]) {
 
       int indexValue = (int) blockNumber;
 
-      oldTagValue = cache[indexValue];
+      oldTagValue = cache[indexValue][0];
 
       if (oldTagValue == -1) {
          hitOrMiss = "Miss";
-         cache[indexValue] = tagValue;
+         cache[indexValue][0] = tagValue;
          misses++;
       } else {
          if (tagValue == oldTagValue) {
@@ -127,7 +177,7 @@ int main(int argc, char *argv[]) {
             hits++;
          } else {
             hitOrMiss = "Miss";
-            cache[indexValue] = tagValue;
+            cache[indexValue][0] = tagValue;
             misses++;
          }
       }
@@ -137,11 +187,11 @@ int main(int argc, char *argv[]) {
       if (tracing) {
          if (oldTagValue == -1) {
             printf("|%8x|%8x|%8x|%8s|%5s|%7d|%7d|%8d|%.8f|", 
-               hexAddress, tagValue, blockNumber, " ", hitOrMiss, hits, 
+               hexAddress, tagValue, setNumber, " ", hitOrMiss, hits, 
                misses, accesses, missRatio);
          } else {
             printf("|%8x|%8x|%8x|%8d|%5s|%7d|%7d|%8d|%.8f|", 
-               hexAddress, tagValue, blockNumber, oldTagValue, hitOrMiss, hits, 
+               hexAddress, tagValue, setNumber, oldTagValue, hitOrMiss, hits, 
                misses, accesses, missRatio);
          }
       
@@ -157,7 +207,8 @@ int main(int argc, char *argv[]) {
 
                                      /* Print the final output of the program */
    printf("\nName: Michael A. Mendoza\n");
-   printf("Parameters: %s %s %s %s\n", argv[1], argv[2], argv[3], argv[4]);
+   printf("Parameters: %s %s %s %s %s %s\n", argv[1], argv[2], argv[3], 
+      argv[4], argv[5], argv[6]);
    printf("Hits: %d\n", hits);
    printf("Misses: %d\n", misses);
    printf("Miss Ratio: %0.8f\n", missRatio);
